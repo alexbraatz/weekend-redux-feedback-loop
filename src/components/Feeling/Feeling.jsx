380dc7348a07f7d68c176e33897ffe7739213d3f
alex braatz <alexjbraatz@gmail.com>
2021-04-03 14:11:19 -0500

    add feeling page component

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,13 @@
+function Feeling(){
+    return(
+        <>
+            <h2>How are you feeling today?</h2>
+
+            <lable>Feeling?</lable><br />
+            <input type='number'></input>
+            <button>Next</button>
+        </>
+    )
+};
+
+export default Feeling;
