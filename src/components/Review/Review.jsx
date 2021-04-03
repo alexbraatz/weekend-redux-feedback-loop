9592e59527fd4584bd24c2242c033b0e84ddc1d8
alex braatz <alexjbraatz@gmail.com>
2021-04-03 18:56:24 -0500

    review page shows user all inputs they've entered

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,11 @@
-import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { useState } from 'react';
 
 function Review(){
 
-    
-
-    const history = useHistory();
+    const getUserFeedback = useSelector( ( store )=>{
+        return store.userGiveFeedback
+    })
 
     const goToThankYou = () => {
         history.push( '/thankyou' );
@@ -14,10 +14,10 @@ function Review(){
         <>
             <h2>Review Your Feedback</h2>
             <div>
-                <p>Feelings:</p>
-                <p>Understanding:</p>
-                <p>Support:</p>
-                <p>Comments:</p>
+                <p>Feelings: { getUserFeedback.feeling }</p>
+                <p>Understanding: { getUserFeedback.understanding }</p>
+                <p>Support: { getUserFeedback.support }</p>
+                <p>Comments: { getUserFeedback.comments }</p>
             </div>
             <button onClick={ (event) => goToThankYou() }>Submit</button>
         </>
