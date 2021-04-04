0689eede79de39876022c04cff2af8cf2d3f91f8
alex braatz <alexjbraatz@gmail.com>
2021-04-04 00:20:10 -0500

    rendering to DOM all feedback items

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -25,16 +25,15 @@ function Admin(){
                     </tr>
                 </thead>
                 <tbody>
-                    {userFeedback.map( feedback => { feedback.map( user=>{     
+                    {userFeedback.map( feedback => { return feedback.map( user=>{     
                         console.log( 'in user:', user )                
                         return (
                             <>
-                            <h2>{ JSON.stringify( {user} )}</h2>
-                                <tr key={  user.id  }>
-                                    <td>{ user.feeling }</td>
+                                <tr>
+                                    <td key={  user.id  }>{ user.feeling }</td>
                                     <td>{ user.understanding }</td>
                                     <td>{ user.support }</td>
-                                    <td>{ user.commments }</td>
+                                    <td>{ user.comments }</td>
                                     <td><button>Delete</button></td>
                                 </tr>
                             </>
